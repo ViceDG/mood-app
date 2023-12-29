56a5b51d8e3b6255e00016cae4c09cbabc219916
Angel <angel.l.puente@gmail.com>
2023-12-29 15:00:09 -0600

    set up Info folder/js file

---
+++ screens/Info/Info.js
@@ -0,0 +1,29 @@
+import { useState, useCallback, useRef } from "react";
+import { View, Text, TouchableOpacity, ScrollView , SafeAreaView} from "react-native";
+import { useFonts } from "expo-font";
+import InfoStyles from "./Info.styles";
+
+
+
+
+const InfoScreen = () => {
+
+  
+    return (
+    <SafeAreaView style={InfoStyles.selectContainer}>
+      <ScrollView>
+                <Text
+          style={{
+            ...InfoStyles.headerText,
+            fontFamily: "Baloo-Bhaijaan2",
+          }}
+        >
+          About
+        </Text>
+
+      </ScrollView>
+      </SafeAreaView>
+    );
+  };
+  
+  export default InfoScreen;
--- utils/MyStack.js
+++ utils/MyStack.js
@@ -4,6 +4,7 @@ import { createStackNavigator } from "@react-navigation/stack";
 import HomeScreen from "../screens/HomeScreen/HomeScreen";
 import SceneSelect from "../screens/SceneSelect/SceneSelect";
 import SceneView from "../screens/SceneView/SceneView";
+import InfoScreen from "../screens/Info/Info";
 
 const Stack = createStackNavigator();
 
@@ -26,6 +27,7 @@ const MyStack = () => {
       <Stack.Screen name="Home" component={HomeScreen} />
       <Stack.Screen name="SceneSelect" component={SceneSelect} />
       <Stack.Screen name="SceneView" component={SceneView} />
+      <Stack.Screen name="InfoScreen" component={InfoScreen}/>
     </Stack.Navigator>
   );
 };
