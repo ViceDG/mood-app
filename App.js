9b34b20ac99f82bf9c259924115b2f8af106bf76
“julian <badjab326@gmail.com>
2024-01-09 09:45:16 -0800

    update: statusbar fix

--- App.js
+++ App.js
@@ -2,12 +2,14 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import MyStack from "./utils/MyStack";
-import 'react-native-reanimated'
-import 'react-native-gesture-handler'
+import { StatusBar } from "expo-status-bar";
+import "react-native-reanimated";
+import "react-native-gesture-handler";
 
 export default function App() {
   return (
     <NavigationContainer>
+      <StatusBar style="dark" translucent backgroundColor="transparent" />
       <MyStack />
     </NavigationContainer>
   );
